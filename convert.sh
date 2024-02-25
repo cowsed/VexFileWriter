1ba1eb5be9b3bfdbaab47d3ef71245fb25214cd5
cowsed <richie.sommers@outlook.com>
2024-02-25 17:35:52 -0500

    light edits

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-xd -dfilebuffer $1 > include/file.h
 
 name=$(basename $1)
 
-echo "const char * fname = \"$name\";" >> include/file.h
+echo "const char * fname = \"$name\";" > include/file.h
 
+xd -dfilebuffer $1 >> include/file.h
--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,21 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-#include "vex.h"
 #include "file.h"
+#include "vex.h"
 vex::competition Competition;
 
 void pre_auton(void) {}
 void autonomous(void) {}
 
-
 vex::brain Brain;
-
 
 void usercontrol() {
   vex::brain Brain;
   Brain.SDcard.savefile(fname, filebuffer, sizeof(filebuffer));
 
   printf("Finished writing");
-
+  fflush(stdout);
   // vexSystemErrorMessageSet("asdsadfasfdsa");
 }
-int main() {
-
-  usercontrol();
-}
+int main() { usercontrol(); }
